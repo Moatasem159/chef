c5c2a196f8a6bef612cb4607b624e21fbcfe59af
Moatasem mohamed <moatasemmohamed39@gmail>
2024-06-27 22:52:11 +0300

    - add dependencies

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChefBot extends StatelessWidget {
   const ChefBot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+    return const ScreenUtilInit(
+      designSize: Size(360, 736),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(),
+      ),
     );
   }
 }
