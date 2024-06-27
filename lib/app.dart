7a4fd515969e809be398f415fd42d7affac39870
Moatasem mohamed <moatasemmohamed39@gmail>
2024-06-27 17:28:56 +0300

    - initial commit - setup app - setup firebase

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChefBot extends StatelessWidget {
+  const ChefBot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
