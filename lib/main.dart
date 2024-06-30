5d6368869940378b2c44317f790828d0ffa30355
Moatasem mohamed <moatasemmohamed39@gmail>
2024-06-30 13:35:24 +0300

    - fix screen util issue

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:chef/core/helpers/constants.dart';
 import 'package:chef/core/helpers/shared_preferences_helper.dart';
 import 'package:chef/core/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   final bool isOnboardingSkipped =await SharedPrefHelper.getBool(AppConstants.skippedOnboarding);
   runApp(
     ChefBot(
