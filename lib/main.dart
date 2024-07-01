56a352dad43c25fa9698352c0cd749305e97e5c7
Moatasem mohamed <moatasemmohamed39@gmail>
2024-07-01 21:29:27 +0300

    -add network info class

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,19 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 late FirebaseUserModel? loggedInUser;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupGetIt();
-  loggedInUser=getIt<Box<FirebaseUserModel>>().get(AppConstants.loggedInUser);
-  final bool isOnboardingSkipped =await SharedPrefHelper.getBool(AppConstants.skippedOnboarding);
+  loggedInUser = getIt<Box<FirebaseUserModel>>().get(AppConstants.loggedInUser);
+  final bool isOnboardingSkipped = await SharedPrefHelper.getBool(AppConstants.skippedOnboarding);
+  final bool isUserLoggedIn = loggedInUser == null ? false : true;
   runApp(
     ChefBot(
-      appRouter: AppRouter(isOnboardingSkipped: isOnboardingSkipped,isUserLoggedIn: loggedInUser==null?false: true),
+      appRouter: AppRouter(
+        isOnboardingSkipped: isOnboardingSkipped,
+        isUserLoggedIn: isUserLoggedIn,
+      ),
     ),
   );
 }
